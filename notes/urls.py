87344d98872e974a44cfac8ad1870fa75543d51c
scott062 <scott.m.burdick@gmail.com>
2024-11-04 14:28:23 -0500

    Remove redundant notes/ from url routing

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from notes.views import NoteListView, NoteDetailView
 
 urlpatterns = [
-    path('notes/', NoteListView.as_view(), name='note_list'),
-    path('notes/<uuid:pk>/', NoteDetailView.as_view(), name='note_detail'),
+    path('', NoteListView.as_view(), name='note_list'),
+    path('<uuid:pk>/', NoteDetailView.as_view(), name='note_detail'),
 ]
